c3e827019c10d38e49325813df7e2acdcd99538e
sivaphani2003 <79915101+sivaphani2003@users.noreply.github.com>
2022-01-29 10:07:14 +0530

    Update week 9 exceptionhandling1.java

--- week 9 exceptionhandling1.java
+++ week 9 exceptionhandling1.java
@@ -1,1 +1,11 @@
-
+class TryCatchExample1 
+{  
+  public static void main(String[] args) 
+  {    
+    int data=50/0; //may throw exception        
+    System.out.println("rest of the code");        
+  }    
+}  
+//throws error and rest of the code is not executed
+//Exception in thread "main" java.lang.ArithmeticException: / by zero
+//         at TryCatchExample1.main(TryCatchExample1.java:7)
