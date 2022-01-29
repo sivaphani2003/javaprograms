a0499754d0847d4bd9489cbe79b2cbb97ca5625c
sivaphani2003 <79915101+sivaphani2003@users.noreply.github.com>
2022-01-29 09:59:41 +0530

    Create week 9 exceptionhandling.java
    try catch block exception handling ---- arithmetic error

---
+++ week 9 exceptionhandling.java
@@ -0,0 +1,19 @@
+classExceptionExample
+{
+  public static void main(String args[])
+  {
+    try
+    {
+      int data=100/0;
+    }
+    catch(ArithmeticException e)
+    {
+      System.out.println(e);
+    }
+    System.out.println("rest of the code...");
+  }
+}
+
+//output
+//java.lang.ArithmeticException: / by zero
+//rest of the code...
